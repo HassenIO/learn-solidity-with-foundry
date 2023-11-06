decf562977611e5c02b5030e2dc6bab2a6cb4a10
Hassen Taidirt <hi@hassen.io>
2023-11-06 22:06:29 +0100

    Rename challenge folder ids with one more 0

--- challenges/0001/tests/Counter.t.sol
+++ challenges/0001/tests/Counter.t.sol
@@ -12,12 +12,12 @@ contract CounterTest is Test {
         counter.setNumber(0);
     }
 
-    function test_001_Increment() public {
+    function test_Increment() public {
         counter.increment();
         assertEq(counter.number(), 1);
     }
 
-    function testFuzz_001_SetNumber(uint256 x) public {
+    function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
     }
